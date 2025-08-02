425426328e9be2ae08a664a88edac4fab2136c20
Jehad-Horani <jehadhorani18@gmail.com>
2025-08-02 20:24:37 +0300

    i

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -206,7 +206,7 @@ export default function Herosec() {
                             rel="noopener noreferrer"
                             className="text-white text-xl ml-2 inline-block hover:text-red-300 transition font-bold"
                         >
-                           Sygnia by Hilton Amman
+                           Signia by Hilton Amman
                         </a>
                     </div>
                 </div>
